7dc61a40d31966be08581d55067207ba2c559029
Andrew Kravtsov <akravtsov@gomel.iba.by>
2021-01-14 11:04:33 +0300

    Task6 - Components & delete button

---
+++ src/components/CardList/Card/CardHeader/index.jsx
@@ -0,0 +1,46 @@
+import { TiPencil } from 'react-icons/ti';
+import { AiOutlineCheck } from 'react-icons/ai';
+import { GiCancel } from 'react-icons/gi';
+import "./index.css";
+
+const cardHeader = props => {
+    if ( props.person.isEditMode ) 
+        return <p>
+            <input 
+                className="head"
+                type="text"
+                value={props.person.name}
+                onChange={( event ) => props.updateProperty( props.person.id, 'name', event.target.value )} />
+            { 
+                !props.isReadOnly?
+                    <span>
+                        <GiCancel 
+                            title="Отменить изменения"
+                            className="icon-button"
+                            onClick={() => props.cancel( props.person.id )} />
+                        <AiOutlineCheck 
+                            title="Сохранить"
+                            className='icon-button'
+                            onClick={() => props.save( props.person.id )} />
+                    </span> : null
+            }
+        </p>;
+    else 
+        return <p className="head">
+            {props.person.name}
+            <input 
+                className="color-switcher" 
+                type="checkbox" 
+                checked={props.person.isChecked} 
+                onChange={() => props.updateProperty( props.person.id, 'isChecked', props.person.isChecked ? false : true )} />
+            { 
+                !props.isReadOnly ?
+                    <TiPencil 
+                        title="Редактировать"
+                        className="icon-button" 
+                        onClick={() => props.editMode( props.person.id )} /> : null
+            }
+    </p>;
+};
+
+export default cardHeader;
